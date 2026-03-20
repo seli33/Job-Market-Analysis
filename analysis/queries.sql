c9fcbb7de8e8d25f3f2c7bae714b92ed6e9bd387
seli <selinamaharjan39@gmail.com>
2026-03-20 21:19:26 +0545

    queries

--- analysis/queries.sql
+++ analysis/queries.sql
@@ -19,3 +19,14 @@ ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER(),1) AS percentage
 FROM jobs
 GROUP BY category
 ORDER BY total_jobs DESC
+
+
+-- =====================================================
+-- 2. LOCATION ANALYSIS
+-- =====================================================
+
+SELECT 
+location, COUNT(*) AS total_jobs
+FROM jobs
+GROUP BY location
+ORDER BY total_jobs DESC;
